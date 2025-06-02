a92062f7d7c8677ea1a0058c4743ded064eab095
Dmitry <mailmetelev@gmail.com>
2025-06-02 09:16:52 +0500

    fix

--- server/api/invalidate.post.ts
+++ server/api/invalidate.post.ts
@@ -1,30 +1,8 @@
-import { defineEventHandler, getQuery, setResponseStatus } from "h3";
+import { defineEventHandler, getQuery } from "h3";
 
 export default defineEventHandler(async (event) => {
-  const { path, token, all } = getQuery(event);
-
-  // Безопасность
-  if (token !== process.env.INVALIDATE_SECRET) {
-    setResponseStatus(event, 401);
-    return { error: "Unauthorized" };
-  }
-
   const cache = useStorage("cache"); // Nitro кэш, работает для isr
 
-  if (all === "true") {
-    await cache.clear();
-    return { ok: true, invalidated: "all" };
-  }
-
-  if (!path) {
-    setResponseStatus(event, 400);
-    return {
-      message: "Path query param required, e.g. ?path=/travel-guides/foo",
-    };
-  }
-
-  await cache.removeItem(path as string, {
-    removeMeta: true,
-  });
-  return { ok: true, invalidated: path };
+  await cache.clear();
+  return { ok: true, invalidated: "all" };
 });
